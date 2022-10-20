7ffb0f71b42c08270e44eb95ae76b73222c2cfed
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-20 13:07:05 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 
+# curl https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/Navid_2022_10_14c_Test/bootstrap.sh | bash
+
 echo 
 echo "Bootstrapping the menu - JamOrHam"
 echo
@@ -9,6 +11,25 @@ sudo apt-get update
 sudo apt-get -y install wget bash
 sudo apt-get -y install dialog
 sudo apt-get install -y  git python gcc g++ make
+
+cat > /tmp/bootstrap_note << EOF
+Shortly after you proceed, the server  will
+automatically reboot and an expected error
+message will appear.
+Please wait 30 seconds before clicking on
+"Retry" to reconnect.
+
+After this, every time you open a terminal,
+a menu will offer all the available options.
+
+To proceed, press Enter.
+
+EOF
+
+cd /tmp
+clear
+dialog --textbox bootstrap_note 15 53
+clear
 
 cd /
 if [ ! -s xDrip ]
@@ -20,16 +41,15 @@ if [ ! -s scripts ]
 then
 sudo mkdir scripts
 fi
+
 cd /tmp
-if [ ! -s update_scripts.sh ]
-then
-#wget https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/Navid_2022_10_14c/update_scripts.sh # Navid's
+sudo rm update_scripts.sh
+#wget https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/Navid_2022_10_14c_Test/update_scripts.sh # Navid's
 wget https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/update_scripts.sh # Main
 if [ ! -s update_scripts.sh ]
 then
 echo "UNABLE TO DOWNLOAD update_scripts SCRIPT! - cannot continue - please try again!"
 exit 5
-fi
 fi
 
 sudo chmod 755 update_scripts.sh
@@ -40,7 +60,7 @@ sudo /xDrip/scripts/update_scripts.sh
 
 # So that the menu comes up as soon as the user logs in (opens a terminal)
 cd /tmp
-cat > start_menu.sh << EOF
+cat > /tmp/start_menu.sh << EOF
 #!/bin/sh
 /xDrip/scripts/menu.sh
 
