59d55334f6ea79b9a215ca965777191b49450b97
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-20 16:47:52 -0400

    Update Status.sh
    MongoDB version added

--- Status.sh
+++ Status.sh
@@ -8,5 +8,8 @@ free -h | sed -n 3p >> tmp
 echo "--------------------------------------------------" >> tmp
 df -m . >> tmp
 echo "--------------------------------------------------" >> tmp
+echo "MongoDB" >> tmp
+mongod --version | sed -n 1p >> tmp
+echo "--------------------------------------------------" >> tmp
 
-dialog --textbox tmp 12 50
+dialog --textbox tmp 15 50
