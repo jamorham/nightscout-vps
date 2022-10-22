86a0ac056ca6bc5a79900de091d87f2931828e6c
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-21 21:05:01 -0400

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -4,26 +4,17 @@ echo
 echo "Nightscout variables"
 echo
 
-cd /tmp
-
-cat> /tmp/variablesNote<<EOF
-You will be editing the textfile containing the variables.
-The key combinations for each text editor function will be shown
-at the bottom of the screen.
-^ represents the control key.  Therefore, ^X means pressing
-the control and x keys simultaneously.
-
-To save, press the control and o keys simultaneously.
-Then, press enter to save.
-
-After editing and saving the variables file, you will need to
-reboot the server for the changes to take effect.
-
-Press Enter now to proceed to the text editor.
-EOF
-
-clear
-dialog --textbox variablesNote 18 67
+dialog --msgbox "You will be editing the file containing the variables.\n\
+The key combinations for text editor functions will be shown\n\
+at the bottom of the screen.\n\
+^ represents the control key.  Therefore, ^X means pressing\n\
+the control and x keys simultaneously.\n\n\
+To save, press the control and o keys simultaneously.\n\
+Then, press enter to save.\n\n\
+After editing and saving the variables file, you will need to\n\
+reboot the server for the changes to take effect.\n\n\
+Press Enter now to proceed to the text editor." 18 67
 clear
 
 sudo nano /etc/nsconfig
+ 
