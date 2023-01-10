46187566656c7d35f7331d39bd80dffed1646797
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-10 13:19:59 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,23 +9,18 @@ do
 
 clear
 Choice=$(dialog --colors --nocancel --nook --menu "\
-      \Zr Developed by the xDrip team \Zn\
-  \n\n
+      \Zr Developed by the xDrip team \Zn\n\n
 Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\n" 24 50 14\
+Press Enter to execute the highlighted option.\n\n" 19 50 9\
  "1" "Status"\
- "2" "Installation phase 1 - 15 minutes"\
- "3" "Installation phase 2 - 5 minutes"\
- "4" "Edit variables"\
- "5" "Edit variables in a browser"\
- "6" "Copy data from another Nightscout"\
- "7" "Update scripts"\
- "8" "Backup MongoDB"\
- "9" "Restore MongoDB backup"\
- "10" "FreeDNS Setup"\
- "11" "Customize Nightscout"\
- "12" "Reboot server (Nightscout)"\
- "13" "Exit to shell (terminal)"\
+ "2" "Logs"\
+ "3" "Edit variables"\
+ "4" "Backup MongoDB"\
+ "5" "Restore MongoDB"\
+ "6" "Update platform"\
+ "7" "Utilities"\
+ "8" "Reboot server (Nightscout)"\
+ "9" "Exit to shell (terminal)"\
  3>&1 1>&2 2>&3)
 
 case $Choice in
@@ -35,27 +30,23 @@ case $Choice in
 ;;
 
 2)
-sudo /xDrip/scripts/NS_Install.sh
+clear
+dialog --colors --title "\Zr Developed by the xDrip team \Zn"   --textbox /xDrip/Logs 26 74 
 ;;
 
 3)
-sudo /xDrip/scripts/NS_Install2.sh
-;;
-
-4)
-/xDrip/scripts/variables.sh
-;;
-
-5)
 /xDrip/scripts/varserver.sh
 ;;
 
-6)
-clear
-sudo /xDrip/scripts/clone_nightscout.sh
+4)
+/xDrip/scripts/backupmongo.sh
 ;;
 
-7)
+5)
+/xDrip/scripts/restoremongo.sh
+;;
+
+6)
 cd /srv
 cd "$(< repo)"  # Go to the local database
 sudo git reset --hard  # delete any local edits.
@@ -66,24 +57,11 @@ clear
 sudo /xDrip/scripts/update_scripts.sh
 ;;
 
+7)
+/xDrip/scripts/Utilities.sh
+;;
+
 8)
-/xDrip/scripts/backupmongo.sh
-;;
-
-9)
-/xDrip/scripts/restoremongo.sh
-;;
-
-10)
-clear
-sudo /xDrip/scripts/ConfigureFreedns.sh
-;;
-
-11)
-sudo /xDrip/scripts/update_nightscout.sh
-;;
-
-12)
 dialog --colors --yesno "     \Zr Developed by the xDrip team \Zn\n\n\
 Are you sure you want to reboot the server?\n
 If you do, all unsaved open files will close without saving.\n"  10 50
@@ -96,7 +74,7 @@ sudo reboot
 fi
 ;;
 
-13)
+9)
 cd /tmp
 clear
 dialog --colors --msgbox "        \Zr Developed by the xDrip team \Zn\n\n\
