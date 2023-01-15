e26f0532364931c7a0f14d17546b0086b4077edb
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-15 15:36:57 -0500

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -8,36 +8,55 @@ echo
 
 sudo apt-get update
 
+# vis
 whichpack=$(which vis)
 if [ "$whichpack" = "" ]
 then
   sudo apt-get -y install vis
 fi
 
+# nano
 whichpack=$(which nano)
 if [ "$whichpack" = "" ]
 then
   sudo apt-get -y install nano
 fi
 
+# screen
 whichpack=$(which screen)
 if [ "$whichpack" = "" ]
 then
   sudo apt-get -y install screen
 fi
 
+# jq
 whichpack=$(which jq)
 if [ "$whichpack" = "" ]
 then
   sudo apt-get -y install jq
 fi
 
+# qrencode
 whichpack=$(which qrencode)
 if [ "$whichpack" = "" ]
 then
   sudo apt-get -y install qrencode
 fi
- 
+
+# mongo
+whichpack="$(mongod --version | sed -n 1p)"
+if [ ! "${whichpack%%.*}" = "db version v3" ]
+then
+  sudo apt-get -y install mongodb-server
+fi
+
+# node
+whichpack=$(node -v)
+if [ ! "${whichpack%%.*}" = "v14" ]
+then
+  curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - &&\
+  sudo apt-get install -y nodejs
+fi 
 
 # Add log
 rm -rf /tmp/Logs
