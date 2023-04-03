65fe9fc2f20d499f15a7929755b48dad70963795
Navid <51497406+Navid200@users.noreply.github.com>
2023-04-03 15:12:12 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -66,8 +66,7 @@ ls > /tmp/repo
 sudo mv -f /tmp/repo .    # The repository name is now in /srv/repo
 cd "$(< repo)"
 sudo git checkout vps-1  # ✅✅✅✅✅ Main - Uncomment before PR.
-#sudo git checkout SingleQuotes_Test  # ⛔⛔⛔⛔⛔ For test - Comment out before PR.
-# Navid_Promote_vps_2023_04_03
+sudo git checkout RemoveFreeDNS_Test  # ⛔⛔⛔⛔⛔ For test - Comment out before PR.
 
 sudo git branch > /tmp/branch
 grep "*" /tmp/branch | awk '{print $2}' > /tmp/brnch
