3afa0bf181df29e95f14545755b90ad19d70b172
Navid <51497406+Navid200@users.noreply.github.com>
2023-10-25 14:04:42 -0400

    Update menu_GC_Setup.sh

--- menu_GC_Setup.sh
+++ menu_GC_Setup.sh
@@ -9,8 +9,8 @@ Choice=$(dialog --colors --nocancel --nook --menu "\
         \Zr Developed by the xDrip team \Zn\n\n
 Use the arrow keys to move the cursor.\n\
 Press Enter to execute the highlighted option.\n" 17 50 7\
- "1" "Install Nightscout phase 1 - 15 minutes"\
- "2" "Install Nightscout phase 2 - 5 minutes"\
+ "1" "Install Nightscout phase 1 - 30 minutes"\
+ "2" "Install Nightscout phase 2 - 10 minutes"\
  "3" "Update platform"\
  "4" "Bootstrap the stable release"\
  "5" "Bootstrap the dev. release (advanced)"\
